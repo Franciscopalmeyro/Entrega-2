44bf13c3f9e2e3397802ef2bf4e265771ebdffd8
franciscopalmeyro <franciscopalmeyro@gmail.com>
2022-12-12 23:31:14 -0300

    Segunda Entrega JS e-comerce

---
+++ stock.js
@@ -0,0 +1,56 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "1",
+        tipo: "gaseosa",
+        descripcion: " ) Gaseosa Helada y refrescante",
+        precio: 300,
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "2",
+        tipo: "snacks",
+        descripcion: "  ) Papas Fritas un clásico de las picadas",
+        precio: 500,
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "3",
+        tipo: "fiambres",
+        descripcion: "  ) Salame de la Colonia, una delicia de la comunidad Alpina",
+        precio: 1500,
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "4",
+        tipo: "fiambres",
+        descripcion: "  ) Jamon Crudo: Amor en fetas de las españas mediterraneas",
+        precio: 900,
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "5",
+        tipo: "fiambres",
+        descripcion: "  ) Queso Casero, El mejor amigo de tu juntada",
+        precio: 700,
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "6",
+        tipo: "fiambres",
+        descripcion: "  ) Bondiola, directo del animal mágico de Homero",
+        precio: 500,
+        cantidad:0
+    }
+]
